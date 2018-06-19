80fd44e3ea72b8df2d44ae7d60b8d1a94471be2f
Michael Sauter <michael.sauter@boehringer-ingelheim.com>
2018-06-19 09:03:13 +0200

    Enable forward delete on external keyboard

--- zsh/sourced/keys.zsh
+++ zsh/sourced/keys.zsh
@@ -3,3 +3,6 @@ bindkey "^[b" backward-word
 
 bindkey '^[^[[D' backward-word # macOS / tmux
 bindkey '^[^[[C' forward-word # macOS / tmux
+
+bindkey "^[[3~" delete-char
+bindkey "^[3;5~" delete-char
