396b033c01ba36484089edcec58fc588c64add97
Michael Sauter <mail@michaelsauter.net>
2020-10-30 15:09:16 +0100

    GREP_OPTIONS is deprecated
    ... and I don't use grep anyway.

--- zsh/sourced/colors.zsh
+++ zsh/sourced/colors.zsh
@@ -7,10 +7,6 @@ export LSCOLORS='Gxfxcxdxbxegedabagacad'
 # Enable ls colors
 ls --color -d . &>/dev/null 2>&1 && alias ls='ls --color=tty' || alias ls='ls -G'
 
-# Enable color in grep
-export GREP_OPTIONS='--color=auto'
-export GREP_COLOR='3;33'
-
 # base16
 BASE16_SHELL=$HOME/.config/base16-shell/
 [ -n "$PS1" ] && [ -s $BASE16_SHELL/profile_helper.sh ] && eval "$($BASE16_SHELL/profile_helper.sh)"
