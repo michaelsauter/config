830a4041ea3c88266e23738bcddfb9169e44f986
Michael Sauter <michael.sauter@boehringer-ingelheim.com>
2022-10-26 12:42:51 +0200

    Update base16_eighties setup

--- zsh/sourced/colors.zsh
+++ zsh/sourced/colors.zsh
@@ -9,4 +9,7 @@ ls --color -d . &>/dev/null 2>&1 && alias ls='ls --color=tty' || alias ls='ls -G
 
 # base16
 BASE16_SHELL=$HOME/.config/base16-shell/
-[ -n "$PS1" ] && [ -s $BASE16_SHELL/profile_helper.sh ] && eval "$($BASE16_SHELL/profile_helper.sh)"
+[ -n "$PS1" ] && \
+    [ -s "$BASE16_SHELL/profile_helper.sh" ] && \
+        source "$BASE16_SHELL/profile_helper.sh"
+base16_eighties
